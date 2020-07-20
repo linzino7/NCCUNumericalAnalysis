44c68e5339b9fa80f887027df797343121f27bef
linzino <linzino7@gmail.com>
2020-07-20 15:39:49 +0800

    Add files via upload

--- W3U09Big O.py
+++ W3U09Big O.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul 20 10:44:54 2020
+Created on Mon Jul 20 15:14:45 2020
 
 @author: ASUS
-"""
 
+http://moocs.nccu.edu.tw/media/19147
+"""
 import numpy as np
 
 def f(x):
@@ -20,17 +21,18 @@ def df(x):
     return np.exp(x)-2
 
 # 起始值
-x0 = 2   #x0
-tel = 0  #逼近目標 容忍值
+x0 = -0.5   #x0
+tel = 10**(-14)  #逼近目標 容忍值  ** 代表指數的意思
 i=0      #計算圈數
+c = list()
 
 while abs(f(x0))> tel:
     # 牛頓法公式
     x1 = x0 - (f(x0)/df(x0))
+    # 如果2次方會收斂到一個常數 ，他的收斂數度就是2次方
+    c.append(abs(x1)/abs(x0)**2) 
     x0 = x1
     i+=1
 
-print('跑了幾圈',i)
-print('x0 = ',x0)
-print('f(x0) = ',f(x0))
-print('format f(x0) = ',format(f(x0), '.25f'))
+print(c)
+
